1c2f409bf294163c2c22e1f486c25539b9d4499b
Aditya Advani <aditya@hindusoftware.com>
2017-01-11 02:08:22 +0000

    got mysqldump command to work really well

--- lib.py
+++ lib.py
@@ -8,12 +8,15 @@ import os
 #import ruamel.yaml
 from time import gmtime, strftime
 import yaml
+import MySQLdb
 
 # the following line allows local imports from pwd
 if __package__ is None:
     sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-config = {'db_dump_location':'/srv/www/dbdumps'}
+configFileStream = file('config.yml', 'r')
+config = yaml.load(configFileStream)
+
 
 @task
 def get_strftime(timePattern="%Y%m%d"):
@@ -41,10 +44,34 @@ def run_shell_command(cmdstr):
     return exitcode, out, err
 
 @task
-def dump_mysql_db(dbName, dbUsername="root", dbPassword="123", dbHost="localhost", dumpFolder=config['db_dump_location']):
+def dump_mysql_db(dbName, dbUsername=config["db_server_root_username"], dbPassword=config["db_server_root_password"], dbHost=config["db_server"], dumpFolder=config["db_dump_location"]):
 	""" Function to dump mysql db to dump folder """
 	dumpFilename = "{dbName}_{timeStr}.sql".format(dbName=dbName,timeStr=get_strftime())
 	dumpAbsolutePath = os.path.join(dumpFolder, dumpFilename)
 	print(dumpAbsolutePath)
-	run_shell_cmd("mysqldump -u {dbUsername} -p{dbPassword} -h {dbHost} > {dumpAbsolutePath}".format(dbUsername=dbUsername, dbPassword=dbPassword, dbHost=dbHost, dumpAbsolutePath=dumpAbsolutePath))
-#	pass
+	try:
+		run_shell_cmd("mysqldump -u {dbUsername} -p'{dbPassword}' -h {dbHost} {dbName} > {dumpAbsolutePath}".format(dbUsername=dbUsername, dbPassword=dbPassword, dbHost=dbHost, dbName=dbName, dumpAbsolutePath=dumpAbsolutePath))
+		print("successfully dumped {dbName} to {dumpAbsolutePath}".format(dbName=dbName, dumpAbsolutePath=dumpAbsolutePath))
+	except: # catch *all* exceptions
+		e = sys.exc_info()[0]
+		print("Error: {exception}".format(exception=e))
+
+
+
+@task
+def show_databases(dbUsername=config["db_server_root_username"], dbPassword=config["db_server_root_password"], dbHost=config["db_server"], dumpFolder=config["db_dump_location"]):
+	db = MySQLdb.connect(
+		host=dbHost,
+		user=dbUsername,
+		passwd=dbPassword,
+		db="mysql")
+	
+	cur = db.cursor()
+	# Use all the SQL you like
+	cur.execute("SHOW DATABASES;")
+	
+	for row in cur.fetchall():
+		print(row[0])
+	
+	db.close()
+
