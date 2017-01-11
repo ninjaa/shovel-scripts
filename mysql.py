7f7bdaff57898a8a0e1122fa99fd467a41610bc5
Aditya Advani <aditya@hindusoftware.com>
2017-01-11 21:23:14 +0000

    mysql.show_databases now returns a list instead of printing blindly

--- mysql.py
+++ mysql.py
@@ -37,6 +37,23 @@ def dump(dbName, dbUsername=config["db_server_root_username"], dbPassword=config
         e = sys.exc_info()[0]
         print("Error: {exception}".format(exception=e))
 
+#@task
+#def import(dbName, dumpFilename, dbUsername=config["db_server_root_username"], dbPassword=config["db_server_root_password"], dbHost=config["db_server"], dumpFolder=config["db_dump_location"]):
+#    """ Function to import mysql db from dumpfile """
+#
+#    # if dumpFilename argument is an all numeric date suffix eg 20161111
+#    # create dumpFilename as eg dbName_2016111
+#    if len(dumpFilename) == 8 and dumpFilename.isdigit():
+#        dumpFilename = dbName + "_" + dumpFilename
+#
+#    # tacks on sql file extension if forgotten
+#    if len(dumpFilename) > 4 and dumpFilename[-4:] !== ".sql":
+#        dumpFilename = dumpFilename + ".sql"
+#
+#    dumpAbsolutePath = os.path.join(dumpFolder, dumpFilename)
+#    print(dumpAbsolutePath)
+#
+#
 
 @task
 def show_databases(dbUsername=config["db_server_root_username"], dbPassword=config["db_server_root_password"], dbHost=config["db_server"], dumpFolder=config["db_dump_location"]):
@@ -50,7 +67,13 @@ def show_databases(dbUsername=config["db_server_root_username"], dbPassword=conf
     # Use all the SQL you like
     cur.execute("SHOW DATABASES;")
 
+    mysqlDatabases = []
     for row in cur.fetchall():
-        print(row[0])
+        dbName = row[0]
+        mysqlDatabases.append(dbName)
 
     db.close()
+    
+    print("\n".join(mysqlDatabases))
+    return mysqlDatabases
+
